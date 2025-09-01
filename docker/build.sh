44b32f1113dbd9b15cf7c539c268bd25f97c7356
Stoyan Krastev (skkdevcraft) <163518570+skkdevcraft@users.noreply.github.com>
2025-09-01 16:22:20 +0300

    create a docker image from the source

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,5 @@
 docker build -t audio-enhance -f Dockerfile ..
 
 docker run --gpus all -it --rm -v C:\work\resemble-enhance-docker\input:/workspace/input -v C:\work\resemble-enhance-docker\output:/workspace/output_enhance audio-enhance bash
+
+docker run --gpus all -it --rm -v C:\work\resemble-enhance-docker\input:/workspace/input -v C:\work\resemble-enhance-docker\output:/workspace/output_enhance audio-enhance bash -c "./convert_audio.sh m4a"
